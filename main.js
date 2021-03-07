c65daff96bcecfce01dbafbc01ce7f59a0497862
Golam Rasul <golam.rasul@mztech.us>
2021-03-07 19:21:14 +0600

    elseif added

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-let random = Math.random()
-if (random >0.5) {
-    console.log("your number is greater than 0.5")
-    console.log(random);
-}
+// let random = Math.random()
+// if (random <0.5) {
+//     console.log("your number is less than 0.5")
+//     console.log(random);
+// }
 
-if (random >= 0.5) {
-    console.log("your number is greater than 0.5")
-    console.log(random);
-}
+// if (random >= 0.5) {
+//     console.log("your number is greater than 0.5")
+//     console.log(random);
+// }
 
+
+
+const dayOfWeek = 'thursday'
+
+if (dayOfWeek === 'sunday') {
+    console.log("sunday make me exusted")
+} else if (dayOfWeek === 'friday' ) {
+    console.log("yap i love friday")
+}else if (dayOfWeek === 'thursday' ) {
+    console.log("i love thursday specially after work")
+}
