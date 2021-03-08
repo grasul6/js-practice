435d9d298d9c565b61edfc2dfbda371ce8583f38
Golam Rasul <golam.rasul@mztech.us>
2021-03-08 23:28:52 +0600

    elseif condition added

--- main.js
+++ main.js
@@ -11,12 +11,30 @@
 
 
 
-const dayOfWeek = 'thursday'
+// const dayOfWeek = 'thursday'
 
-if (dayOfWeek === 'sunday') {
-    console.log("sunday make me exusted")
-} else if (dayOfWeek === 'friday' ) {
-    console.log("yap i love friday")
-}else if (dayOfWeek === 'thursday' ) {
-    console.log("i love thursday specially after work")
+// if (dayOfWeek === 'sunday') {
+//     console.log("sunday make me exusted")
+// } else if (dayOfWeek === 'friday' ) {
+//     console.log("yap i love friday")
+// }else if (dayOfWeek === 'thursday' ) {
+//     console.log("i love thursday specially after work")
+// }
+
+
+// 0-2 - free - infant 
+// 3-12 - $10 - child 
+// 13-60 - $20 - adult
+// 60+   - $10 - senior 
+
+const age = 61;
+
+if (age<3) {
+    console.log(" you are infant. you get in for free");
+}else if (age<13) {
+    console.log("you are child, you need to pay $10");
+}else if (age<60) {
+    console.log("you are adult, you need to pay $20");
+}else if (age>60) {
+    console.log("you are senior, you need to pay $10"); 
 }
