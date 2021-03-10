3a1160e2ee34389fb04270cc5536147701513d50
Golam Rasul <golam.rasul@mztech.us>
2021-03-10 16:29:34 +0600

    2nd condition committed at app.js

--- app.js
+++ app.js
@@ -24,3 +24,20 @@ if (random<0.5) {
 }else {
     console.log("your number is greater than  0.5");
 }
+
+
+
+const password = prompt("please enter a new password ");
+
+// password must be 6+ character
+
+
+if (password.length >= 6) {
+    if (password.indexOf(' ') === -1) {
+       console.log("valid password!"); 
+    }else {
+        console.log("password cannot contain spaces!");
+    }
+}else {
+    console.log("password too short! must be 6+ character");
+}
