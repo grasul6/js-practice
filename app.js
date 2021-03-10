5f87832e1457b4aa7fc3cc7910ec5594e11cd37f
Golam Rasul <golam.rasul@mztech.us>
2021-03-10 16:19:27 +0600

    first nest at new branch at app.js branch

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ if (lastName.length >= 6 && lastName.indexOf('  ') === -1) {
 }else{
     console.log("incorrect format for last name");
 }
+
+const firstName = prompt("enter your firstName");
+
+if (firstName.length >= 6 && firstName.indexOf('  ') === -1) {
+    console.log("valid first name");
+}else{
+    console.log("incorrect format for first name");
+}
